b21711740e72d0ef74c221dca634eacdf3e37fd4
ayran-alves <ayrancruzalves8@gmail.com>
2025-09-18 15:40:00 -0300

    Add files via upload

---
+++ lista12.java
@@ -0,0 +1,8 @@
+package primeiroProjeto;
+public class Contador {
+    public static void main(String[] args) {
+        for (int i = 10; i <= 30; i++) {
+            System.out.println(i);
+        }
+    }
+}
