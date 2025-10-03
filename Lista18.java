73058abfd364d285d8a407d9e5abd6be1e66d79b
ayran-alves <ayrancruzalves8@gmail.com>
2025-10-03 16:28:43 -0300

    Add files via upload

---
+++ Lista18.java
@@ -0,0 +1,56 @@
+package primeiroProjeto;
+
+public class Lista18 {
+
+	
+	//1 
+	public static void main(String[] args) {
+		nome();
+		nome();
+		nome();
+		nome();
+		nome();
+ 
+
+		frase();
+		
+		contagem();
+		
+	    vogais();
+		
+	}
+	
+
+		public static void nome () {
+			System.out.println("Ayran Alves");
+		
+		
+	}
+		
+//2
+		
+			public static void frase() {
+			System.out.println("Nunca desista");
+			
+			
+			
+			}
+			
+//3      
+			public static void contagem () {
+				 for(int i = 1; i <= 10; i++) {
+				 System.out.println(i);
+			}
+			}
+				 
+//4				 
+	 
+				 public static void  vogais() {
+					 System.out.println("A E I O U");
+				 
+}
+				 
+//5
+				 
+			
+}
